3acec281098246ab36c4c3084e43067e9e4ebc65
siva prasad k <153076202+sivaprasadkvd@users.noreply.github.com>
2023-12-26 11:50:36 +0530

    Add files via upload

---
+++ WebAPIsaini/WebAPIsaini/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIsaini.Data;
+
+namespace WebAPIsaini.Controllers
+{
+    [ApiController]
+    [Route("api/[contoller]")]
+
+    
+
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbcontext _dbcontext;
+
+        public ContactsController(ContactsAPIDbcontext dbcontext) 
+        {
+                this._dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetContacts()
+        {
+            return Ok(_dbcontext.Contacts.ToList());
+        }
+    }
+}
---
+++ WebAPIsaini/WebAPIsaini/Data/ContactsAPIDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIsaini.Models;
+
+namespace WebAPIsaini.Data
+{
+    public class ContactsAPIDbcontext : DbContext
+    {
+        public ContactsAPIDbcontext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
